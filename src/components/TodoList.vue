269667205ce53f63e96a2513b4f810393c88c52a
isneverdead <isneverdead@github.com>
2020-05-05 13:13:37 +0700

    first commit app todolist

---
+++ src/components/TodoList.vue
@@ -0,0 +1,236 @@
+<template>
+  <div class="container">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+    <h1>Vue ToDo List</h1>
+    
+    <input type="text" class="todo-input" placeholder="What's your plan?"
+    v-model="todo" @keyup.enter="addTodo">
+    
+    <transition-group name="fade">
+    <!-- <transition-group name="fade" enter-active-class="animated fadeInUp faster" leave-active-class="animated fadeOutDown fast"> -->
+    <div class="todo-item" v-for="todo in todosFiltered" :key="todo.id" >
+        
+        <div class="todo-item-left">
+            <button class="todo-item-label" :class="{ completed : todo.completed }" @click="check(todo)">[___]</button>
+            <!-- <input @click="check(todo)" type="checkbox" v-model="todo.completed" > -->
+            
+            <div v-if="!todo.editing" @dblclick="editTodo(todo)" 
+            class="todo-item-label" :class="{ completed : todo.completed }">{{ todo.title }}</div>
+            
+            <input v-else class="todo-item-edit" type="text" 
+            v-model="todo.title" @blur="doneEdit(todo)" 
+            @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)"
+            v-focus>
+        </div>
+        
+        <div class="remove-item" @click="removeTodo(todo.id)">
+            &times;
+        </div>    
+    </div>
+    </transition-group>
+
+    <div class="extra-container">
+        <div><label>
+            <button class="check-all" :class="{ active: checkAll }" @click="checkAllTodos">Check All</button>
+        </label></div>
+        <div>{{ remaining }} item left</div>
+    </div>
+
+    <div class="extra-container">
+        <div>
+            <button :class="{ active: filter == 'all' }" @click="filter = 'all'">All</button>
+            <button :class="{ active: filter == 'active' }" @click="filter = 'active'">Active</button>
+            <button :class="{ active: filter == 'completed' }" @click="filter = 'completed'">Completed</button>
+        </div>
+
+        <div> 
+            <button v-if="showClearCompletedButton" @click="clearCompleted">Clear Completed</button>
+        </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { db } from '../firebaseSetting'
+
+export default {
+  name: 'TodoList',
+  
+  data () {
+    return {
+        todo: '',
+        todos: [],
+        
+        checkAll: false,
+        beforeEditCache: '',
+        filter: 'all',
+        }
+    },
+    firestore: {
+        todos: db.collection('todos'),
+        
+    },
+    computed: {
+        remaining() {
+            return this.todos.filter(todo => !todo.completed).length
+        },
+        anyRemaining() {
+            return this.remaining == 0
+        },
+        todosFiltered() {
+            if (this.filter == 'all') {
+                return this.todos
+            } else if (this.filter == 'active') {
+                return this.todos.filter(todo => !todo.completed)
+            } else if (this.filter == 'completed') {
+                return this.todos.filter(todo => todo.completed)
+            }
+        },
+        showClearCompletedButton() {
+            return this.todos.filter(todo => todo.completed).length > 0
+        },
+        
+    },
+    directives: {
+        focus: {
+            inserted: function (el) {
+                el.focus()
+            }
+        }
+    },
+    methods: {
+        addTodo() {
+            if (this.todo.trim().length == 0) {
+                return
+            }
+            const ref = db.collection('todos').doc()
+            ref.set({
+                id: ref.id,
+                title: this.todo,
+                completed: false,
+                editing: false,
+            })
+            this.todo = ''
+        },
+        editTodo(todo) {
+            this.beforeEditCache = todo.title
+            todo.editing = true
+        },
+        doneEdit(todo) {
+            if (todo.title.trim().length == 0) {
+                todo.title = this.beforeEditCache
+            }
+            const ref = db.collection('todos').doc(todo.id)
+            ref.set({
+                id: ref.id,
+                title: todo.title,
+                completed: false,
+                editing: false
+            })
+        },
+        cancelEdit(todo) {
+            todo.title = this.beforeEditCache
+            todo.editing = false
+        },
+        removeTodo(id) {
+            db.collection('todos').doc(id).delete()
+        },
+        check(todo) {
+            console.log("check : "+todo.completed)
+            const ref = db.collection('todos').doc(todo.id)
+            ref.set({
+                id: ref.id,
+                title: todo.title,
+                completed: !todo.completed,
+                editing: false
+            })
+        },
+        checkAllTodos() {
+            this.checkAll = !this.checkAll
+            this.todos.forEach((todo)=>{
+                console.log("check : "+todo.completed)
+                const ref = db.collection('todos').doc(todo.id)
+                ref.set({
+                    id: ref.id,
+                    title: todo.title,
+                    completed: this.checkAll,
+                    editing: false
+                })
+            })
+        },
+        clearCompleted() {
+            this.todos.forEach((todo) => {
+                if (todo.completed) this.removeTodo(todo.id)
+            })
+        },
+        finishedEdit(data) {
+            this.todos.splice(data.index, 1, data.todo)
+        }
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+    .todo-input {
+        width: 100%;
+        padding: 10px 18px;
+        font-size: 18px;
+        margin-bottom: 16px;
+    }
+    .todo-item {
+        margin-bottom: 12px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        animation-duration: 1s;
+    }
+    .remove-item {
+        cursor: pointer;
+        margin-left: 14px;
+    }
+    .todo-item-left {
+        display: flex;
+        align-items: center;
+    }
+    .todo-item-label {
+        padding: 10px;
+        border: 1px solid white;
+        margin-left: 12px;
+    }
+    .todo-item-edit {
+        font-size: 24px;
+        color: #2c3e50;
+        margin-left: 12px;
+        width: 100%;
+        padding: 10px;
+        border: 1px solid #ccc;
+        font-family: 'Avenir', Arial, Helvetica, sans-serif;
+    }
+    .completed {
+        text-decoration: line-through;
+        color: gray;
+    }
+    .extra-container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-size: 16px;
+        border-top: 1px solid lightgray;
+        padding-top: 14px;
+        margin-bottom: 14px;
+    }
+    .check-all {
+        background-color: white;
+    }
+    button {
+        font-size: 14px;
+        background-color: white;
+        appearance: none;
+    }
+    
+    .active {
+        background: lightgreen;
+    }
+</style>
---
+++ src/firebaseSetting.js
@@ -0,0 +1,16 @@
+import Firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Get a Firestore instance
+export const firebase = Firebase
+  .initializeApp({ 
+    apiKey: "AIzaSyAZsOSIiS1aFYP09RTZm2lR2YuLfFcy0iA",
+    authDomain: "vue-todolist-app.firebaseapp.com",
+    databaseURL: "https://vue-todolist-app.firebaseio.com",
+    projectId: "vue-todolist-app",
+    storageBucket: "vue-todolist-app.appspot.com",
+    messagingSenderId: "820343771205",
+    appId: "1:820343771205:web:d773e58be4a11e2f9f932c"
+  })
+export const db = firebase.firestore()
