f21cc942968541a0f9da9b9acec15ad57d4b52e9
isneverdead <isneverdead@github.com>
2020-05-15 19:45:32 +0700

    Finishing 4 and add login using twitter

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -87,7 +87,7 @@ var uiConfig = {
     // list provider yang akan digunakan (disable yang tidak perlu)
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     // firebase.auth.GithubAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.PhoneAuthProvider.PROVIDER_ID
